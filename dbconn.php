ef45db8299e8fb0bd409e6b639b9948ea102e0a6
Sonia Wronkowska <sonia.wronkowska@gmail.com>
2016-07-24 15:38:20 +0200

    added dbconn

---
+++ dbconn.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = 'localhost';
+$username = 'shop_user';
+$password = 'password';
+$db_name = 'Sklep_internetowy';
+
+$conn = new mysqli($host, $username, $password, $db_name);
+
+
+if($conn->connect_error){
+    die('Nieudane polaczenie. Blad: ' .$conn->connect_error);
+}
+
+echo 'Polaczenie udane.';
+
+$conn->close();
+$conn = null;
