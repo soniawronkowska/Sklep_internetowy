35aece013549d59991c3928dfa2652248a6b4705
Sonia Wronkowska <sonia.wronkowska@gmail.com>
2016-07-24 16:01:23 +0200

    changed name

--- dbconn.php
+++ dbconn.php
@@ -1,12 +1,11 @@
 <?php
 
-$host = 'localhost';
+$host = 'localhosts';
 $username = 'shop_user';
 $password = 'password';
 $db_name = 'Sklep_internetowy';
 
 $conn = new mysqli($host, $username, $password, $db_name);
-
 
 if($conn->connect_error){
     die('Nieudane polaczenie. Blad: ' .$conn->connect_error);
